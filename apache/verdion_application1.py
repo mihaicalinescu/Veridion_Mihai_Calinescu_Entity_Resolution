11cd320b749ec4e61c6189c2885af4e6e1755d35
Mihai Calinescu <m.calinescu18@gmail.com>
2025-03-26 15:26:00 +0200

    First update, but it takes too long to present a result

--- apache/verdion_application1.py
+++ apache/verdion_application1.py
@@ -9,8 +9,20 @@ cols = ['company_name', 'company_legal_names', 'website_domain', "main_city", "m
 # Removing duplicates
 df = df[cols].drop_duplicates()
 
-# Normalization of company name and website domains
-df["company_name_clean"] = df['company_name'].str.lower().str.replace('^a-z0-9', "", regex=True)
+# filling nans and convert to string
+df["company_name"] = df['company_name'].fillna("").astype(str)
+df["website_domain"] = df['website_domain'].fillna("").astype(str)
+
+# create clean columns
+df["company_name_clean"] = (
+    df["company_name"].str.lower()
+    .str.replace("[^a-z0-9]", "", regex=True)
+)
+
+df["website_domain_clean"] = (
+    df["website_domain"].str.lower()
+    .str.replace("www.|http://|https://", "", regex=True)
+)
 
 
 def find_duplicates(df, threshold=85):
@@ -18,18 +30,22 @@ def find_duplicates(df, threshold=85):
     for i in range(len(df)):
         for j in range(i+1, len(df)):
             # conditions for avoiding unnecessary comparison
-            same_country = df.iloc[i]['main_country_code'] == df.iloc[j]['main_country_code']
-            same_city = df.iloc[i]['main_city'] == df.iloc[j]['main_city']
+            name_i = df.iloc[i]["company_name_clean"]
+            name_j = df.iloc[j]["company_name_clean"]
+
+            if not name_i or not name_j:
+                continue
+
+            same_country = df.iloc[i]["main_country_code"] == df.iloc[j]["main_country_code"]
+            same_city = df.iloc[i]["main_city"] == df.iloc[j]["main_city"]
+
 
             if same_country and same_city:
-                name_similarity = fuzz.ratio(
-                    df.iloc[i]['company_name_clean'],
-                    df.iloc[j]['company_name_clean']
-                )
-                website_match = df.iloc[i]['website_domain_clean'] == df.iloc[j]['website_domain_clean']
+                name_similarity = fuzz.ratio(name_i, name_j)
+                website_match = df.iloc[i]["website_domain_clean"] == df.iloc[j]["website_domain_clean"]
 
                 if name_similarity > threshold or website_match:
-                    duplicate.append((i, j, name_similarity, website_match))
+                    duplicates.append((i, j, name_similarity, website_match))
 
     return duplicates
 
@@ -42,3 +58,4 @@ for i, j, similarity, website_match in duplicates:
     print(f"- {df.iloc[i]['company_name']} ({df.iloc[i]['main_city']}, {df.iloc[i]['website_domain']})")
     print(f"- {df.iloc[j]['company_name']} ({df.iloc[j]['main_city']}, {df.iloc[j]['website_domain']})")
     print(f"Similaritate nume: {similarity}%, Website match: {website_match}")
+
